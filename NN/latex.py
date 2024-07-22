1d2a506bf7c72bdda92d6e4933ddea04a60a395a
Ahmad-server <pouramini@gmail.com>
2024-07-22 16:47:08 +0330

    commit

--- NN/latex.py
+++ NN/latex.py
@@ -22,6 +22,7 @@ def generate_latex_table(table, caption="table", label=""):
         previous_model = None
         row_span = 0
 
+        row_spans = []
         for i, model in enumerate(model_col):
             if model == previous_model:
                 multirow_model_col.append('')
@@ -29,6 +30,7 @@ def generate_latex_table(table, caption="table", label=""):
             else:
                 if row_span > 0:
                     multirow_model_col[-(row_span+1)] = f"\\multirow{{{row_span+1}}}{{*}}{{{previous_model}}}"
+                    row_spans.append(row_span)
                 row_span = 0
                 multirow_model_col.append(model)
             previous_model = model
@@ -40,6 +42,14 @@ def generate_latex_table(table, caption="table", label=""):
 
     # Convert to LaTeX table using tabulate
     latex_table = tabulate(escaped_table, headers='keys', tablefmt='latex_raw', showindex=False)
+
+    # Insert \hline at the beginning and after each group of model names
+    latex_table_lines = latex_table.splitlines()
+    for i in range(len(latex_table_lines) - 2, 1, -1):
+        if not latex_table_lines[i].startswith('\\multirow') and latex_table_lines[i+1].startswith('\\multirow'):
+            latex_table_lines.insert(i+1, '\\hline')
+
+    latex_table = '\n'.join(latex_table_lines)
 
 
     table_env = f"""
